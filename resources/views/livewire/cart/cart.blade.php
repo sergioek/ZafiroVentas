a6fe32a6305d730eb47f3c26aaf70f8fc6ca66b8
sergioek <khairallahsergio4@gmail.com>
2021-09-10 17:58:55 -0300

    Carrito:mejorandolo

--- resources/views/livewire/cart/cart.blade.php
+++ resources/views/livewire/cart/cart.blade.php
@@ -1,5 +1,7 @@
 <div>
 <x-alerts/>
+<x-button-create href="{{route('products.index')}}"/>
+<br>
 <div class="table table-hover table-responsive">
     <table class="col-lg-12">
         <thead class="table-dark">
@@ -47,19 +49,33 @@
                             <button class="btn btn-danger"><i class="far fa-trash-alt" wire:click="DeleteAmount({{$cart}})"></i></button>
                     </td>
               </tr>
+
         
               @empty
-              <a href="{{route('products.index')}}">
-                <div class="alert alert-warning alert-dismissible fade show" role="alert">
-                    <i class="fas fa-shopping-cart"><strong>  Carrito Vacio!</strong></i> Agregue productos al carrito
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
+                <a href="{{route('products.index')}}">
+                
+                    <div class="card bg-info">
+                        <h5 class="card-header text-light">Carrito de compras</h5>
+                        <div class="card-body">
+                          <h5 class="card-title text-light">Carrito Vacio!</h5>
+                          <p class="card-text text-light">Por favor agregre un producto.</p>
+                          <br>
+                          <a href="{{route('products.index')}}" class="btn btn-warning">Agregar productos</a>
+                        </div>
+                      </div>
                 </a>
+                  
               @endforelse
           </tbody>
     </table>
+</div>
+
+<div class="container">
+    <div class="row align-items-start">
+
+    <div class="col"><strong>Interes %</strong><input type="number" name="" id="" class="form-control" placeholder="Aplique un porcentaje" min="0"></div>
+    <div class="col"><strong>Descuento %</strong><input type="number" name="" id="" class="form-control" placeholder="Aplique un porcentaje" min="0"></div>
+    </div>
 </div>
 
 </div>
